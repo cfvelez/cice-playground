91be67e7fcbd940cff4d13d5d4a1ea72a0178d45
Carlos Velez <cfvelez9@gmail.com>
2020-03-03 21:51:25 +0100

    MyTodoList - practice

--- modules/10-reactjs/my-todo-list/src/App.tsx
+++ modules/10-reactjs/my-todo-list/src/App.tsx
@@ -1,26 +1,35 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { TodoList } from './features/todo/ui/todo-list/todo-list'
+import { Todo } from './features/todo/domain/todo'
+import { TodoCreate } from './features/todo/ui/todo-create/todo-create'
 
 export function App() {
-  const tasks = [
-    { id: 1, name: 'Task 2', status: 0 },
-    { id: 2, name: 'Task 2', status: 0 },
-    { id: 3, name: 'Task 3', status: 0 }
-  ]
+  const [todos, setTodos] = useState<Todo[]>([])
 
-  const [value, setValue] = useState('')
+  function createTodo(todoText: string) {
+    const newTodo = { id: Math.random() * 1000, text: todoText, completed: false }
+    setTodos([...todos, newTodo])
+  }
 
-  console.log(useState(''))
+  function completeTodo(id: number) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+
+        return todo
+      })
+    )
+  }
 
   return (
     <>
-      {tasks.map(t => (
-        <div>{t.name}</div>
-      ))}
-      <input value={value} onChange={event => setValue(event.target.value)} />
+      <TodoList todos={todos} onCompleteTodo={completeTodo}></TodoList>
+      <TodoCreate onCreate={createTodo} />
     </>
   )
 }
-
-export default App
