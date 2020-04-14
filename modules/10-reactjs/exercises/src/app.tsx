b19c04ec95c5f933b7d9d1288ad077221982dce2
Carlos Velez <cfvelez9@gmail.com>
2020-04-14 20:24:51 +0200

    ok

--- modules/10-reactjs/exercises/src/app.tsx
+++ modules/10-reactjs/exercises/src/app.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 import { ExerciseOne } from './exercise-one'
-<<<<<<< HEAD
-=======
 import { ExerciseTwo } from './exercise-two'
 import { ExerciseThree } from './exercise-three'
->>>>>>> upstream/master
+
 import { ExerciseFour } from './exercise-four'
 import { ExerciseFive } from './exercise-five'
 import { ExerciseSix } from './exercise-six'
@@ -16,21 +14,12 @@ export const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Link to="/exercise-one">Exercise one</Link>
-<<<<<<< HEAD
-=======
       <Link to="/exercise-two">Exercise two</Link>
       <Link to="/exercise-three">Exercise three</Link>
->>>>>>> upstream/master
       <Link to="/exercise-four">Exercise four</Link>
       <Link to="/exercise-five">Exercise five</Link>
       <Link to="/exercise-six">Exercise six</Link>
       <Link to="/exercise-seven">Exercise seven</Link>
-<<<<<<< HEAD
-      <Link to="/exercise-eight">Exercise Eight</Link>
-      <Route path="/exercise-one">
-        <ExerciseOne />
-      </Route>
-=======
       <Link to="/exercise-eight">Exercise eight</Link>
 
       <Route path="/exercise-one">
@@ -42,7 +31,6 @@ export const App: React.FC = () => {
       <Route path="/exercise-three">
         <ExerciseThree />
       </Route>
->>>>>>> upstream/master
       <Route path="/exercise-four">
         <ExerciseFour />
       </Route>
@@ -56,11 +44,7 @@ export const App: React.FC = () => {
         <ExerciseSeven />
       </Route>
       <Route path="/exercise-eight">
-<<<<<<< HEAD
-        <ExerciseEight />
-=======
         <ExerciseEight percentage={70} />
->>>>>>> upstream/master
       </Route>
     </BrowserRouter>
   )
